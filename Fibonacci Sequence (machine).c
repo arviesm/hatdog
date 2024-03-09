db5e1dd638d31363b47aa402741e746fb5c17f53
arviesm <meivrasinocruz1904@gmail.com>
2024-03-09 15:31:28 +0800

    initial commit

---
+++ Fibonacci Sequence (machine).c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void printFibonacci(int n) {
+    int a = 1, b = 1, f;
+
+    printf("The Fibonacci Sequence of %d is: ", n);
+
+    while (a <= n) {
+        printf("%d ", a);
+        f = a + b;
+        a = b;
+        b = f;
+    }
+    printf("\n\nProgram Terminated!");
+}
+
+int Fibonacci(int num) {
+    int a = 1, b = 1, f;
+
+    while (a <= num) {
+        if (a == num) {
+            return 1;
+        }
+        f = a + b;
+        a = b;
+        b = f;
+    }
+
+    return 0;   
+    
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    
+    while (1) {
+        scanf("%d", &num);
+
+        if (Fibonacci(num)) {
+  
+            printFibonacci(num);
+            break; 
+        } else {
+            printf("%d is not a  Fibonacci number.\n\nEnter another number: ", num);
+        }
+    }
+
+    return 0;
+}
+
